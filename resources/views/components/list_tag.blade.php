b44c117ddae91f4971019e336a76bdd4e53e4af7
ThaiLe <lethaivp98@gmail.com>
2018-11-20 23:22:12 +0700

    abc

--- resources/views/components/list_tag.blade.php
+++ resources/views/components/list_tag.blade.php
@@ -1,13 +1,13 @@
 <div class="tag">
 	<ul class="tag-list">
 		@for($i=1;$i<=20;$i++)
-		<li class="tag-list__item"><a href="#" title="">PHP</a></li>
+		<li class="tag-list__item"><a href="/key-word-tag" title="">PHP</a></li>
 		@endfor
 		@for($i=1;$i<=20;$i++)
-		<li class="tag-list__item"><a href="#" title="">Laravel</a></li>
+		<li class="tag-list__item"><a href="/key-word-tag" title="">Laravel</a></li>
 		@endfor
 		@for($i=1;$i<=20;$i++)
-		<li class="tag-list__item"><a href="#" title="">Jquery</a></li>
+		<li class="tag-list__item"><a href="/key-word-tag" title="">Jquery</a></li>
 		@endfor
 	</ul>
 </div>
